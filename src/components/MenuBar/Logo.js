9b3d931ca0e7e9178c679a2d352173195be1a248
Heid <haidren.amalia@gmail.com>
2020-03-14 16:23:10 +0800

    Logo update, make it responsive

--- src/components/MenuBar/Logo.js
+++ src/components/MenuBar/Logo.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
-const typoProps = {
-  variant: "h6",
-  style: { display: "inline-block", marginRight: "0.3em" }
-};
+const useStyles = makeStyles(theme => ({
+  typo: {
+    [theme.breakpoints.only("xs")]: {
+      fontSize: "1.1em"
+    }
+  }
+}));
 
 const Logo = () => {
+  const classes = useStyles();
+
+  const typoProps = {
+    variant: "h6",
+    style: { display: "inline-block", marginRight: "0.3em" },
+    className: classes.typo
+  };
   return (
     <div>
       <Typography {...typoProps}>ASTERISK</Typography>
