908c2031e304713ecdfb2d3babaa03722fc24530
Heid <haidren.amalia@gmail.com>
2020-03-14 23:11:24 +0800

    Updates

---
+++ precache-manifest.3589bf51173ff0f05c242551c169f2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81baad53a8fbc178470dd6316fb3cea3",
+    "url": "/asterisk-co/index.html"
+  },
+  {
+    "revision": "8e41ed45e778d3299394",
+    "url": "/asterisk-co/static/js/2.ba4be21d.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/asterisk-co/static/js/2.ba4be21d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435c505bce621abcefb8",
+    "url": "/asterisk-co/static/js/main.6b62c66c.chunk.js"
+  },
+  {
+    "revision": "f4e70aa12a49f9d575e8",
+    "url": "/asterisk-co/static/js/runtime-main.ac0bc6d2.js"
+  },
+  {
+    "revision": "fb558d7af48ba1b0694565e3c75ba214",
+    "url": "/asterisk-co/static/media/arch-lg.fb558d7a.jpg"
+  }
+]);
